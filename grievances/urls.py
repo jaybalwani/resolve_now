0fc18251623f7bc68c247e0d6ff6697a45428315
jaybalwani <jaybalwani007@gmail.com>
2024-06-29 17:00:03 +0530

    Added login/logout

--- grievances/urls.py
+++ grievances/urls.py
@@ -1,9 +1,12 @@
 # grievances/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='grievances/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('submit/', views.submit_grievance, name='submit_grievance'),
     path('grievance_list/', views.grievance_list, name='grievance_list'),
     path('', views.home, name='home'),
